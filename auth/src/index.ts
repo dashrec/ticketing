e1d50a94b0df100d82f0198a6c0c223ed7a191d6
dashrec <shavliashvili@gmx.de>
2022-11-18 22:28:27 +0100

    update auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,16 +6,12 @@ const start= async () => {
 
   if (!process.env.JWT_KEY){
     throw new Error('JWT_KEY must be defined');
-  }
-
-  
+  }  
   if (!process.env.MONGO_URI){
     throw new Error('MONGO_URI must be defined');
   }
-
 try {
   await mongoose.connect(process.env.MONGO_URI, {//auth-mongo-srv is a service name created in auth-mongo-depl.yaml. auth = give this db a name
-
   }); console.log('connected to mongo db');
 }
 catch (err){
