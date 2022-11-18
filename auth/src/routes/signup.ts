6d6815bf3f90695fb5cc03ca22791b51552b4fa7
dashrec <shavliashvili@gmx.de>
2022-11-18 23:05:41 +0100

    update cline signup page

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -19,7 +19,7 @@ async (req: Request, res: Response) => {
    throw new RequestValidationError(errors.array()); //it will be automatically picked up by error-handler middleware 
   }*/
 
-const { email, password }= req.body;
+const { email, password } = req.body;
 const existingUser = await User.findOne({ email });
 if (existingUser) {
   throw new BadRequestError('Email in use');
@@ -29,20 +29,15 @@ if (existingUser) {
 const user = User.build({ email, password });
 await user.save();
 
-
-
 const userJwt = jwt.sign({ //generate json web token
   id: user.id,
   email: user.email
 }, process.env.JWT_KEY! ); //exclamation tells typescript we know 100% that jwt_key is defined
 
-
 //store it on session object
 req.session = { //cookie session library is gonna take it serialize and send it to browser
   jwt:userJwt //base 64 encode
 };
-
-
 
 res.status(201).send(user); //send user to browser
 
